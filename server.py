66cf47d9326299d6fe0303e9ec9b4722042202c0
Snigdha-001 <snigdha@vaidhyamegha.com>
2023-08-07 00:36:31 +0530

    first commit

--- server.py
+++ server.py
@@ -1,19 +1,38 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    first = data.get('first')
+    second = data.get('second')
+    if first is None or second is None:
+        return jsonify(error='Both "first" and "second" numbers must be provided.'), 400
+
+    try:
+        result = float(first) + float(second)
+        return jsonify(result=result), 200
+    except ValueError:
+        return jsonify(error='Invalid input. Both "first" and "second" must be numbers.'), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    first = data.get('first')
+    second = data.get('second')
+    if first is None or second is None:
+        return jsonify(error='Both "first" and "second" numbers must be provided.'), 400
+
+    try:
+        result = float(first) - float(second)
+        return jsonify(result=result), 200
+    except ValueError:
+        return jsonify(error='Invalid input. Both "first" and "second" must be numbers.'), 400
 
 if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+    app.run(port=8080, host='0.0.0.0')
